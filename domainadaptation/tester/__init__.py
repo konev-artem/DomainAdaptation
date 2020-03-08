c5c0650443e82eb4d1786a3f57b03b372f582a44
sava-stepurin <savastep@rambler.ru>
2020-03-08 12:29:25 +0300

    Create structure of project and add tester

---
+++ domainadaptation/tester/__init__.py
@@ -0,0 +1,4 @@
+from .tester import test
+
+
+__all__ = ["test"]
---
+++ domainadaptation/tester/tester.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensorflow as tf
+
+
+def test(model,
+         generator,
+         optimizer,
+         batch_size=32):
+    pass
