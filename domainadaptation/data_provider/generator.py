8ece2bb47d6e7eb520d7825d26e10c9dd298ef74
bixind <vbif-98@mail.ru>
2020-03-23 16:33:01 +0300

    fixed dataprovider

--- domainadaptation/data_provider/generator.py
+++ domainadaptation/data_provider/generator.py
@@ -1,14 +1,19 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
 
 class DomainGenerator:
-    def __init__(self, source_dir, target_dir, target_size=(100, 100)):
-        self.datagen = ImageDataGenerator()
-        self.target_size = target_size
-        self.source_gen = self.datagen.flow_from_directory(source_dir)
-        self.target_gen = self.datagen.flow_from_directory(target_gen)
-        
-    def source_generator():
-        return self.source_gen
-    
-    def target_generator():
-        return self.target_gen
+    # you can use image_data_generator_kwargs for augmentation
+    def __init__(self, dataset_dir, **image_data_generator_kwargs):
+        self.datagen = ImageDataGenerator(**image_data_generator_kwargs)
+        self.domains = dict()
+        with os.scandir(dataset_dir) as it:
+            for entry in it:
+                if entry.is_dir():
+                    self.domains[entry.name] = entry.path
+
+    def get_domain_names(self):
+        return list(self.domains.keys())
+
+    def make_generator(self, domain, **generator_kwargs):
+        dir = self.domains[domain]
+        return self.datagen.flow_from_directory(dir, **generator_kwargs)
