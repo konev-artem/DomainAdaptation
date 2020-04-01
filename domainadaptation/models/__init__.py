e849aa01c31a1747652a467f153ea2a4f84acb80
kailiakeugene@mail.ru <vasya@pupkin.ru>
2020-04-01 08:55:47 +0300

    add flatten, include_top

--- domainadaptation/models/__init__.py
+++ domainadaptation/models/__init__.py
@@ -2,4 +2,4 @@ from .base_model import BaseModel
 from .blocks import GradientReversal
 from .resnet101 import BaseFabric, Resnet101Fabric
 
-__all_ = ['BaseModel', 'GradientReversal', 'Resnet101Fabric', 'BaseFabric']
+__all_ = ['BaseModel', 'GradientReversal', 'Resnet101Fabric', 'VGG19Fabric', 'Resnet50Fabric', 'BaseFabric']
