37ca6ab38c36258be5d0f8051886214af6ac52b6
ekolodin <ekolodin98@gmail.com>
2020-04-07 15:29:29 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import logging
+
+from domainadaptation.experiment import DANNExperiment
+
+if __name__ == '__main__':
+    logging.disable(logging.WARNING)
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    exp = DANNExperiment(config={
+        'backbone': {
+            'type': 'resnet50',
+            'num_trainable_layers': -1,
+            'img-size': (224, 224),
+            'weights': 'imagenet',
+            'pooling': 'max'},
+        'dataset': {
+            'classes': 12,
+            'path': '/content/office-home',
+            'augmentations': {},
+            'source': 'train',
+            'target': 'validation'},
+        'batch_size': 16,
+        'epochs': 10,
+    })
+
+    exp.experiment_domain_adaptation_v2()
