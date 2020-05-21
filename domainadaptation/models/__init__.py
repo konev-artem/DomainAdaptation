6cefb319a4812428dd0e46e428f2674520f7d050
students <jvchizh@almaren.velkerr.ru>
2020-05-21 05:56:22 -0400

    Added visualization to CAN experiment

--- domainadaptation/models/__init__.py
+++ domainadaptation/models/__init__.py
@@ -1,5 +1,4 @@
 from domainadaptation.models.blocks import GradientReversal
-from domainadaptation.models.resnet101 import BaseFabric, Resnet101Fabric
 from domainadaptation.models.alexnet import AlexNet, proprocess_input
 
-__all__ = ['GradientReversal', 'Resnet101Fabric', 'BaseFabric', 'AlexNet']
+__all__ = ['GradientReversal', 'AlexNet']
